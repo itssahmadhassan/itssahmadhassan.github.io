483fc3f42436fe34ca59e4eb927040ded6c43bed
AhmadHassan <itssahmadhassan@gmail.com>
2020-07-27 05:52:50 +0500

    first commit

---
+++ assets/javascript/main.js
@@ -0,0 +1,19 @@
+
+// for contact-icon
+
+function changeContact(){
+
+    document.getElementById("link").innerHTML = "Resume";
+}
+function changeWork(){
+
+    document.getElementById("link").innerHTML = "Work";
+}
+function changeAbout(){
+
+    document.getElementById("link").innerHTML = "About";
+}
+
+function backLink(){
+    document.getElementById("link").innerHTML = "Home";
+}
